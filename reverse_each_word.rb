08c5d684c2b29788aed7e84ed6245f34ae33f3ba
Davis Brown <cooliomoded@gmail.com>
2020-08-13 21:09:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 require 'pry'
 
-def reverse_each_word(sentence_string)
-  sentence_array = sentence_string.split(" ")
+def reverse_each_word(string)
+  sentence_array = string.split(" ")
   reversed_array = []
-  sentence_array.each |sentence_string| do
+  sentence_array.each do |sentence_string|
     reversed_array << sentence_string.reverse
   end
   reversed_array.join(" ")
