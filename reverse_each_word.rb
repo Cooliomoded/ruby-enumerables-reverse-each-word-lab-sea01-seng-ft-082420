6e0f1714ba176b4477efbda0535051d8a72dd272
Davis Brown <cooliomoded@gmail.com>
2020-08-13 20:39:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(string)
-  string.each |string| do
-    string.collect |letter| do
-      letter.shift
-    end
+def reverse_each_word(sentence_string)
+  i = 0
+  array = []
+  while i < sentence_string.length do
+    array.unshift[sentence_string[i]]
   end
 end
